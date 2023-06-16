6f55c7efd8613b23b19247683b914cfef6aa2aef
frannaddy <francisaddy90@gmail.com>
2023-06-16 06:37:04 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <studio.h>
+/**
+ * main - program prints out all single numbers of base
+ * Return: Always 0 (success)
+ */
+int mai(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		printf("%d", n);
+	printf("\n");
+	return (0);
+}
+
