97f2e91ecf3fd8f7bb9e392264b2cbb1b001ebc7
frannaddy <francisaddy90@gmail.com>
2023-06-21 14:25:20 -0700

    like and comment

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,18 @@
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+
+	i = 0;
+
+        printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}
