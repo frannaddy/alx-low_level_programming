8e21d49e25b2b3105464c0b062b0e6eeb213e02e
frannaddy <francisaddy90@gmail.com>
2023-08-07 07:01:08 -0700

    task0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,18 +1,16 @@
 #include "main.h"
 
 /**
- * read_textfile - reads a file text to be printed out
- * @filename: name of the file to read
- *
+ * read_textfile - reads the file and prints it to teh standard output
+ * @filename: file to be printed
  * @letters: number of letters to read and print
  *
  * Return: the number of letters printed, or 0 if it failed
  */
 ssize_t read_textfile(const char *filename, size_t letters)
-
 {
 	int fd;
-	int i, j;
+	int s, t;
 	char *buf;
 
 	if (!filename)
@@ -26,23 +24,23 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (!buf)
 		return (0);
 
-	i = read(fd, buf, letters);
-	if (i < 0)
+	s = read(fd, buf, letters);
+	if (s < 0)
 	{
 		free(buf);
 		return (0);
 	}
-	buf[i] = '\0';
+	buf[s] = '\0';
 
 	close(fd);
 
-	j = write(STDOUT_FILENO, buf, i);
-	if (j < 0)
+	t = write(STDOUT_FILENO, buf, s);
+	if (t < 0)
 	{
 		free(buf);
 		return (0);
 	}
 
 	free(buf);
-	return (j);
+	return (t);
 }
