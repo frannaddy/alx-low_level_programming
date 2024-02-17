e824e5dfacd1de761b0c96a033d9a2bbb2f50e24
frannaddy <francisaddy2247@gmail.com>
2024-02-17 01:57:37 -0800

    bit manipulation

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,17 +1,27 @@
 #include "main.h"
 
 /**
- * get_bit - return value of a bit at a given index
- * @n: decimal parameter
- * @index: index
- * Return: val
+ * get_bit - returns the value of a bit at a given
+ * index.
+ * @n: unsigned long int input.
+ * @index: index of the bit.
+ *
+ * Return: value of the bit.
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	int bit;
+	unsigned int i;
 
-	bit = (n >> index);
-	if (index > 32)
+	if (n == 0 && index < 64)
+		return (0);
+
+	for (i = 0; i <= 63; n >>= 1, i++)
+	{
+		if (index == i)
+		{
+			return (n & 1);
+		}
+	}
+
 	return (-1);
-	return (bit & 1);
 }
--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,16 +1,22 @@
 #include "main.h"
-#include <stdlib.h>
 
 /**
- * set_bit - sets the value of a bit to 1 at a given index
- * @n: the value whose bit is to be set to 1
- * @index: position of the bit
- * Return: 1 if it worked, -1 if error occured
+ * set_bit - sets the value of a bit to 1.
+ * at a given index.
+ * @n: pointer of an unsigned long int.
+ * @index: index of the bit.
+ *
+ * Return: 1 if it worked, -1 if it didn't.
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (index > sizeof(n) * 8)
+	unsigned int m;
+
+	if (index > 63)
 		return (-1);
-	*n |= (1 << index);
+
+	m = 1 << index;
+	*n = (*n | m);
+
 	return (1);
 }
